b9d7a12b98fa160bcbfe301d71b3a917424ebc7b
alyamtiara <mutiara260707@gmail.com>
2026-06-25 19:38:49 +0700

    Create DatabaseManager.java

---
+++ src/DatabaseManager.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+
+    // TODO: Change these values based on your database configuration
+    private static final String URL = "jdbc:mysql://localhost:3306/game_project";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Ayaya260707";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
